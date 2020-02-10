a83e5f883b5286fee9ec31d62ecb1e1f7493d33d
Peter Glennon <pcrglennon@gmail.com>
2020-02-10 13:03:23 -0500

    CORS fix: allow OPTIONS requests

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module RecipeAppApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get]
+        resource '*', headers: :any, methods: [:get, :options]
       end
     end
   end
