7dfcc7802b4a35e0fa8532baf63184f01fc9ee10
Peter Glennon <pcrglennon@gmail.com>
2020-02-16 13:35:32 -0500

    Configure CORS to allow POSTs, for GraphQL

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module RecipeAppApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
